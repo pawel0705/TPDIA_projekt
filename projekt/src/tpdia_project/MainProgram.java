52c5bc4ce28b6ef659dd3215b6e9ae4ad8bd6ad1
pawel0705 <pawel.salicki@gmail.com>
2022-05-19 14:52:46 +0200

    added weka library

--- projekt/src/tpdia_project/MainProgram.java
+++ projekt/src/tpdia_project/MainProgram.java
@@ -1,8 +1,19 @@
 package tpdia_project;
 
+import java.io.File;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.classifiers.trees.RandomForest;
+
 public class MainProgram {
 
 	public static void main(String[] args) {
+
 		 System.out.println("Hello, World!"); 
 	}
 
