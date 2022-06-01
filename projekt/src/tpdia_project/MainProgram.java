56be59c46a774c5ee2be15696c67b18f0b954b43
Mateusz Chłopek <matikc25@gmail.com>
2022-06-01 20:47:23 +0200

    Summing the extracted features to one CSV file

--- projekt/src/tpdia_project/MainProgram.java
+++ projekt/src/tpdia_project/MainProgram.java
@@ -1,6 +1,8 @@
 package tpdia_project;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -75,6 +77,44 @@ public class MainProgram {
 		System.out.println("---- Done training model ----");
 		System.out.println("Total files count: " + filesOpenedIterator);
 		System.out.println("Number of files that could not be opened: " + failureInterator);
+		
+		Instances summedFeatures = GetFeaturesFromAllDataSets();
+	}
+	
+	private static boolean ProcessOneDataset(String domainName, String fileName, List<Integer> measuresColumns) 
+	{
+
+		boolean success = true;
+
+		String fullDatasetPath = "datasets/" + domainName + "/" + fileName + ".csv";
+
+		Instances dataset;
+		dataset = csvManager.GetDataSet(fullDatasetPath);
+
+		if (dataset == null) {
+			success = false;
+			System.out.println("There was a problem reading the file: " + fullDatasetPath + ". Skipping..");
+			return success;
+		}
+
+		System.out.println("----- File: " + fullDatasetPath + " -----");
+		
+		int attributesNumber = dataset.numAttributes();
+		Features[] features = new Features[attributesNumber];
+		
+		for (int columnNr = 0; columnNr < attributesNumber; columnNr++) 
+		{
+			System.out.println("---- Column nr.: " + (columnNr + 1));
+			features[columnNr] = new Features(dataset, columnNr, measuresColumns.contains(columnNr + 1));
+		}
+		
+		SaveFeaturesToCSV(features, "extractedFeatures/", fileName);
+		
+		// Some data about CSV
+		System.out.println();
+		System.out.println(dataset.toSummaryString());
+
+		return success;
 	}
 
 	private static void SaveFeaturesToCSV(Features[] features, String directory, String fileName)
@@ -118,38 +158,60 @@ public class MainProgram {
 		}
 	}
 	
-	private static boolean ProcessOneDataset(String domainName, String fileName, List<Integer> measuresColumns) {
-
-		boolean success = true;
-
-		String fullDatasetPath = "datasets/" + domainName + "/" + fileName + ".csv";
-
-		Instances dataset;
-		dataset = csvManager.GetDataSet(fullDatasetPath);
-
-		if (dataset == null) {
-			success = false;
-			System.out.println("There was a problem reading the file: " + fullDatasetPath + ". Skipping..");
-			return success;
-		}
-
-		System.out.println("----- File: " + fullDatasetPath + " -----");
-		
-		int attributesNumber = dataset.numAttributes();
-		Features[] features = new Features[attributesNumber];
-		
-		for (int columnNr = 0; columnNr < attributesNumber; columnNr++) 
+	private static Instances GetFeaturesFromAllDataSets() 
+	{
+		File extractedFeaturesFilesDirectory = new File("extractedFeatures/");
+		File[] CSVs = extractedFeaturesFilesDirectory.listFiles();
+		try 
 		{
-			System.out.println("---- Column nr.: " + (columnNr + 1));
-			features[columnNr] = new Features(dataset, columnNr, measuresColumns.contains(columnNr + 1));
+			File csv = new File("summed.csv");
+			csv.createNewFile();
+			
+			FileWriter writer = new FileWriter(csv);
+			writer.write("DataType,"
+					+ "PositiveValueRatio,NegativeValueRatio,ZeroValueRatio,"
+					+ "UniqueValueRatio,"
+					+ "SameDigitalNumber,"
+					+ "Average,Minimum,Maximum,Median,UpperQuartile,LowerQuartile,"
+					+ "VariationCoefficient,"
+					+ "RangeRatio,"
+					+ "LocationRatio,"
+					+ "NumericalColumnRatio,"
+					+ "NumericalNeighbour,"
+					+ "IsMeasure"
+					+ "\n");
+		
+		
+			for(int i = 0; i < CSVs.length; ++i)
+			{
+				System.out.println(CSVs[i].getName());
+				BufferedReader br = new BufferedReader(new FileReader(CSVs[i]));
+				
+				String line = br.readLine();
+				int lineCounter = 0;
+				while(line != null)
+				{
+					//System.out.println(line + "\n");
+					if(lineCounter++ == 0)
+					{
+						line = br.readLine();
+						continue;
+					}
+					
+					writer.write(line + "\n");
+					
+					line = br.readLine();
+				}
+			}
+			
+			writer.close();
+			
+			return csvManager.GetDataSet("extractedFeatures/summed.csv");
+		} 
+		catch(IOException e)
+		{
+			
 		}
-		
-		SaveFeaturesToCSV(features, "extractedFeatures/", fileName);
-		
-		// Some data about CSV
-		System.out.println();
-		System.out.println(dataset.toSummaryString());
-
-		return success;
+		return null;
 	}
 }
